c3ae6290b20146245e64eb42323da6a6ec86bd0f
Bắc Lục <45648505+NosuGurin@users.noreply.github.com>
2018-12-11 17:21:36 +0700

    Commit 2

---
+++ DocGhiFile/DocGhiFile/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using DocGhiFile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocGhiFile.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET
+        public ActionResult Register()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        public ActionResult Register(UserInfo model)
+        {
+            //Lưu thông tin vào session
+            Session["user"] = model;
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            //Chuyển về trang đăng ký nếu không có session
+            if (Session["user"] == null)
+            {
+                return RedirectToAction("Register", "Account");
+            }
+            return View();
+        }
+        public ActionResult LogOff()
+        {
+            Session.Remove("user");
+            return RedirectToAction("Register", "Account");
+        }
+    }
+}
